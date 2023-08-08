3950ffd547dc1f6085dd688f9b24a38e2caa5597
taehoon14 <141623405+taehoon14@users.noreply.github.com>
2023-08-08 18:02:12 +0900

    Add files via upload

---
+++ 2일차/새 폴더/문제3.py
@@ -0,0 +1,5 @@
+for i in range(10,1,-1):
+    print(i, end='')
+while i>=1:
+    print(i,end='')
+    i=-1
---
+++ 2일차/새 폴더/터틀 모듈.py
@@ -0,0 +1,9 @@
+import turtle
+a=turtle.Pen()
+a.shape('turtle')
+a.color('black')
+a.begin_fill()
+a.circle(130)
+a.end_fill()
+
+turtle.exitonclick()
